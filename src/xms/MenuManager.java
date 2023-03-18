ab6eb095ea7b8f8131e3b35e8dc385ec1f4c9291
hyunsseong <hsan0326@naver.com>
2023-03-18 22:42:06 +0900

    added menumanager.java

--- src/xms/MenuManager.java
+++ src/xms/MenuManager.java
@@ -4,7 +4,11 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("1. Add study");
+		System.out.println("2. Delete study");
+		System.out.println("3. Edit study");
+		System.out.println("4. View study");
+		System.out.println("5. Exit");
 	}
 
 }
