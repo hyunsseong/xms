2dbce521331873b2fbd45cde8081a19c1a160fba
hyunsseong <hsan0326@naver.com>
2023-04-02 00:23:24 +0900

    add menu method

--- src/xms/MenuManager.java
+++ src/xms/MenuManager.java
@@ -15,41 +15,61 @@ public class MenuManager {
 			System.out.println("4. View subject");
 			System.out.println("5. Show a Menu");
 			System.out.println("6. Exit");
-			System.out.print("Select one number : ");
+			System.out.print("Select one number between 1 - 6: ");
 			num = input.nextInt();
-		
+			
+			
 			switch (num) {
 			case 1 :
-				System.out.print("Subject Number : ");
-				int subjectNumber = input.nextInt();
-				input.nextLine();
-				System.out.print("Subject Name : ");
-				String subject = input.nextLine();
-				System.out.print("Exam Date : ");
-				String examDate = input.nextLine();
-				System.out.print("Exam Time : ");
-				String examTime = input.nextLine();
-				System.out.print("Study Plan : ");
-				String studyPlan = input.nextLine();
-				System.out.println();
+				addSubject();
 				break;
 			case 2:
-				System.out.print("Subject Number : ");
-				int deleteSubject = input.nextInt();
+				deleteSubject();
 				break;
 			case 3:
-				System.out.print("Subject Number : ");
-				int editSubject = input.nextInt();
+				editSubject();
 				break;
 			case 4:
-				System.out.print("Subject Number : ");
-				int viewSubject = input.nextInt();
+				viewSubject();
 				break;
 			case 5:
 			case 6:
 				continue;
 			}
 		}
+	}
+	
+	public static void addSubject() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Subject Number : ");
+		int subjectNumber = input.nextInt();
+		input.nextLine();
+		System.out.print("Subject Name : ");
+		String subject = input.nextLine();
+		System.out.print("Exam Date : ");
+		String examDate = input.nextLine();
+		System.out.print("Exam Time : ");
+		String examTime = input.nextLine();
+		System.out.print("Study Plan : ");
+		String studyPlan = input.nextLine();
+	}
+	
+	public static void deleteSubject() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Subject Number : ") ;
+		int subjectNumber = input.nextInt();
+	}
+	
+	public static void editSubject() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Subject Number : ");
+		int subjectNumber = input.nextInt();
+	}
+	
+	public static void viewSubject() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Subject Number : ");
+		int subjectNumber = input.nextInt();
 	}
 }
 
